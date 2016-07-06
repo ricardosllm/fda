9f03bda47077d160516a4457c6303074f7ac474f
Ricardo Mota <ricardosllm@gmail.com>
2016-07-06 22:20:12 +0800

    Fixed issued with db access roles

--- ops/roles/app_stack/templates/engine.config.js
+++ ops/roles/app_stack/templates/engine.config.js
@@ -1,5 +1,7 @@
 var config = {};
 
 config.s3Burl = "https://s3-{{ aws.region }}.amazonaws.com/{{ MainBucket }}/";
+config.resourcesCounterDBName = "fda-{{ app_name }}-{{ db_resources_counter }}";
+config.snapshotDBName = "fda-{{ app_name }}-{{ db_snapshot }}";
 
 module.exports = config;
