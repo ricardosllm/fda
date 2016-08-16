8482a6b566b49fc2a72714551d8c341c178fde0b
Ricardo Mota <ricardosllm@gmail.com>
2016-08-16 20:25:13 +0800

    Finished Restructuring roles

--- roles/app_stack/templates/engine.config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.s3Burl = "https://s3-{{ aws.region }}.amazonaws.com/{{ MainBucket }}/";
-config.resourcesCounterDBName = "fda-{{ app_name }}-{{ db_resources_counter }}";
-config.snapshotDBName = "fda-{{ app_name }}-{{ db_snapshot }}";
-
-module.exports = config;
